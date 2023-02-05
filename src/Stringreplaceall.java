dbc1c6704fb5ffbf65fa694b9a8f391ebd302b26
potthuri-charan <potthuricharanpadmasrikhar@gmail.com>
2023-02-06 00:04:18 +0530

    Update Stringreplaceall.java

--- src/Stringreplaceall.java
+++ src/Stringreplaceall.java
@@ -1,7 +1,7 @@
 public class Stringreplaceall {
     public static void main(String[] args) {
         String s1="This is Charan ";
-        String replaceString=s1.replaceAll("s","w");//replaces all occurrences of "a" to "e"
+        String replaceString=s1.replaceAll("s","w");
         System.out.println(replaceString);
     }
 }
