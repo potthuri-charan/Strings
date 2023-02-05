034839a2b549f83dc9f04d1d0bab486397305de8
potthuri-charan <potthuricharanpadmasrikhar@gmail.com>
2023-02-05 23:35:03 +0530

    Update Stringequalsignorecase.java

--- src/Stringequalsignorecase.java
+++ src/Stringequalsignorecase.java
@@ -1,7 +1,10 @@
-import java.util.Scanner;
-
 public class Stringequalsignorecase {
     public static void main(String[] args) {
-        
+        String a="Charan";
+        String b="CHARAN";
+        String c="Srinivas";
+        String d="SRINIVAS";
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(c.equalsIgnoreCase(d));
     }
 }
